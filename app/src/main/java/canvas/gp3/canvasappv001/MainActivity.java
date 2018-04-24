06230024fa0aa9e8a7a7b250b1c6b5fa5ff13fcd
Kyle MacQueen <36674113+KyleMacQueen@users.noreply.github.com>
2018-04-24 21:38:59 +0100

    Update MainActivity.java

--- app/src/main/java/canvas/gp3/canvasappv001/MainActivity.java
+++ app/src/main/java/canvas/gp3/canvasappv001/MainActivity.java
@@ -2,12 +2,25 @@ package canvas.gp3.canvasappv001;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private DrawingView drawView;   //instance variable
+    private ImageButton currPaint;  //represents paint colour button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        drawView = (DrawingView)findViewById(R.id.drawing); //instantiation of variable by retrieving a reference to it from the layout
+        
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        currPaint = (ImageButton)paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
     }
 }
